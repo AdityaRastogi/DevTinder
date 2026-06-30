8f14dd3e9f7820f64fd6a6b0a0ff5bab796aa2ee
Aditya Rastogi <rastogiaditya11@gmail.com>
2026-07-01 00:15:53 +0200

    chore:create server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use("/test",(req,res) =>{
+    res.send('Hello World from test');
+});
+
+app.use("/hello",(req,res) =>{
+    res.send('Hello hello geelo');
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
